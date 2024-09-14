5ec7e03749db1060e704a8a8243be1c501993fa1
Emin Mammadov <eminmammadovcom@gmail.com>
2024-09-14 17:57:12 +0400

    back-main-update
    I reverted it to its original state and applied the change steps specified in the task.

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
-// #include <stdio.h>
-
 int main()
 {
-  // int i;
-  // I have decided to use the loop variable "int i" only inside the loop. 
-  // This approach makes the code cleaner and more compact. 
-  // The shorter and more efficient the code, 
-  // the more efficient it is in terms of RAM and CPU resources.
-  for (int i = 0; i < 32; i++)
+  int i;
+  for (i = 0; i < 32; i++)
   {
     printf("Hello School\n");
   }
