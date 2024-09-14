aa4672d25a206ef384c0224484d32fb8aa724175
Emin Mammadov <eminmammadovcom@gmail.com>
2024-09-14 17:47:49 +0400

    first-updates
    I have decided to use the loop variable "int i" only inside the loop. This approach makes the code cleaner and more compact. The shorter and more efficient the co the more efficient it is in terms of RAM and CPU resources.

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
+// #include <stdio.h>
+
 int main()
 {
-  int i;
-  for (i = 0; i < 10; i++)
+  // int i;
+  // I have decided to use the loop variable "int i" only inside the loop. 
+  // This approach makes the code cleaner and more compact. 
+  // The shorter and more efficient the code, 
+  // the more efficient it is in terms of RAM and CPU resources.
+  for (int i = 0; i < 32; i++)
   {
-    printf("School\n");
+    printf("Hello School\n");
   }
   return 0;
 }
